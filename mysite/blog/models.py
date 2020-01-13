64ba0f9b9b44fe084806ce345cc843122adf136f
tuanweb <nguyenanhtuan92my@gmail.com>
2020-01-13 10:12:44 +0700

    The 4th time

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -15,7 +15,10 @@ class Post(models.Model):
         self.save()
 
     def approve_comments(self):
-        return self.comments.filter(approved_comments=True)
+        return self.comments.filter(approved_comment=True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail",kwargs={'pk':self.pk})
 
     def __str__(self):
         return self.title
@@ -32,7 +35,8 @@ class Comment(models.Model):
         self.approved_comment = True
         self.save()
 
+    def get_absolute_url(self):
+        return reverse('post_list')
+
     def __str__(self):
         return self.text
-
-        
