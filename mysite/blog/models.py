b81438f825a0ee82377caf0fcb925a4531406e39
tuanweb <nguyenanhtuan92my@gmail.com>
2020-01-13 10:00:55 +0700

    The third time

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils import TIME_ZONE
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post',related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+        
